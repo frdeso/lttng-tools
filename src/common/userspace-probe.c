e549054b646a03cca3cbbb6fd4e26a979cf71482
Francis Deslauriers <francis.deslauriers@efficios.com>
2017-11-28 18:06:50 -0500

    cleanup: Remove usage of probe location specific lookup_method getter
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>

--- src/common/userspace-probe.c
+++ src/common/userspace-probe.c
@@ -708,7 +708,7 @@ end:
 	return ret;
 }
 
-struct lttng_userspace_probe_location_lookup_method *
+static struct lttng_userspace_probe_location_lookup_method *
 lttng_userspace_probe_location_function_get_lookup_method(
 		struct lttng_userspace_probe_location *location)
 {
@@ -725,7 +725,7 @@ end:
 	return ret;
 }
 
-struct lttng_userspace_probe_location_lookup_method *
+static struct lttng_userspace_probe_location_lookup_method *
 lttng_userspace_probe_location_tracepoint_get_lookup_method(
 		struct lttng_userspace_probe_location *location)
 {
